0ba4c7024b8b525e17b1006be941779fb2f5c35e
railgun-1145 <2757796182@qq.com>
2026-04-10 14:27:23 +0800

    Embed UAC + PerMonitorV2 manifest via build.rs; add transglass.manifest; wire self-update hotkey

---
+++ TransGlass/target/debug/build/transglass-0ddff15a69f5a299/build_script_build-0ddff15a69f5a299.d
@@ -0,0 +1,5 @@
+E:\git ku\TransGlass\target\debug\build\transglass-0ddff15a69f5a299\build_script_build-0ddff15a69f5a299.d: build.rs
+
+E:\git ku\TransGlass\target\debug\build\transglass-0ddff15a69f5a299\build_script_build-0ddff15a69f5a299.exe: build.rs
+
+build.rs:
---
+++ TransGlass/target/debug/deps/transglass-cfa5527ff25f68f8.d
@@ -0,0 +1,7 @@
+E:\git ku\TransGlass\target\debug\deps\transglass-cfa5527ff25f68f8.d: src\main.rs
+
+E:\git ku\TransGlass\target\debug\deps\libtransglass-cfa5527ff25f68f8.rmeta: src\main.rs
+
+src\main.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
